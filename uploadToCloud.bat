e9681a6550af3dc96176b3709a0c3f68c1db3ed5
Carlos Santana <cgsp94@gmail.com>
2022-03-11 14:29:42 -0600

    Update

--- uploadToCloud.bat
+++ uploadToCloud.bat
@@ -1,9 +1,4 @@
 @echo off
 scp -i key/id_rsa target\api-0.0.1-SNAPSHOT.jar root@137.184.121.135:/artifact
+echo sango-api /Q
 
-ssh -i key/id_rsa root@137.184.121.135
-
-sudo systemctl stop start-api
-systemctl start start-api.service
-systemctl enable start-api.service
-systemctl status start-api
