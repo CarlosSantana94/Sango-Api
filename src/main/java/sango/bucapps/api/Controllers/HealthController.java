faa65da20364f915114ffd6b703f4ad7c991691a
Carlos Santana <cgsp94@gmail.com>
2023-06-02 12:25:58 -0600

    cancelar orden

--- src/main/java/sango/bucapps/api/Controllers/HealthController.java
+++ src/main/java/sango/bucapps/api/Controllers/HealthController.java
@@ -19,7 +19,7 @@ public class HealthController {
         healthDto.setRequestTime(new Date());
         healthDto.setIsUp(true);
         healthDto.setAppName("SANGO-API");
-        healthDto.setVersion(9);
+        healthDto.setVersion(10);
 
         return healthDto;
     }
@@ -31,7 +31,7 @@ public class HealthController {
         healthDto.setRequestTime(new Date());
         healthDto.setIsUp(true);
         healthDto.setAppName("SANGO-API");
-        healthDto.setVersion(9);
+        healthDto.setVersion(10);
 
         return healthDto;
     }
